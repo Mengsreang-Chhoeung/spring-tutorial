f072f96c8791391fa361df8532b3c332438c2104
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-02-01 20:47:14 +0700

    add setter injection

--- src/com/mengsreang/springXmlConfiguration/PythonTeacher.java
+++ src/com/mengsreang/springXmlConfiguration/PythonTeacher.java
@@ -6,7 +6,7 @@ public class PythonTeacher implements Teacher {
 	
 	private TeacherService teacherService;
 	
-	public PythonTeacher(TeacherService teacherService) {
+	public void setTeacherService(TeacherService teacherService) {
 		this.teacherService = teacherService;
 	}
 
