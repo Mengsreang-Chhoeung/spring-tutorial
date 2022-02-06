ecafca72198a6e767f9e242040b4579c6a84275f
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-02-06 12:43:30 +0700

    done at spring with java source code

--- src/com/mengsreang/springJavaConfiguration/JavaApplication.java
+++ src/com/mengsreang/springJavaConfiguration/JavaApplication.java
@@ -17,6 +17,8 @@ public class JavaApplication {
 		Teacher python = context.getBean("pythonTeacher", Teacher.class);
 		Teacher goTeacher = context.getBean("goTeacher", Teacher.class);
 		Teacher rustTeacher = context.getBean("rustTeacher", Teacher.class);
+		Teacher universityTeacher = context.getBean("universityTeacher", Teacher.class);
+		UniversityTeacher university = context.getBean("universityTeacher", UniversityTeacher.class);
 		
 		// call methods on the bean
 		System.out.println(javaTeacher.getTeaching());
@@ -27,11 +29,16 @@ public class JavaApplication {
 		System.out.println(goTeacher.getTeachingHours());
 		System.out.println(rustTeacher.getTeaching());
 		System.out.println(rustTeacher.getTeachingHours());
+		System.out.println(universityTeacher.getTeaching());
+		System.out.println(universityTeacher.getTeachingHours());
 		
 		boolean isJavaResult = ( javaTeacher == java );
 		boolean isPythonResult = ( pythonTeacher == python );
 		System.out.println("Result of Java: " + isJavaResult);
 		System.out.println("Result of Python: " + isPythonResult);
+		
+		System.out.println("University Teacher Name: " + university.getName());
+		System.out.println("University Teacher Email: " + university.getEmail());
 
 		// close the context
 		context.close();
